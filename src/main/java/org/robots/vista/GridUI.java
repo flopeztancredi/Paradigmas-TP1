c127541fd30cc7909975784f84659f10f16b08b8
echepereza <epereza@fi.uba.ar>
2024-04-27 18:00:29 -0300

    Juego listo (falta estética)

---
+++ src/main/java/org/robots/vista/GridUI.java
@@ -0,0 +1,72 @@
+package org.robots.vista;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
+import org.robots.modelo.Juego;
+
+public class GridUI {
+    private final Juego juego;
+    private final GridPane gridTablero;
+
+    public GridUI(Juego juego, GridPane gridTablero) {
+        this.juego = juego;
+        this.gridTablero = gridTablero;
+    }
+
+    public void iniciarTablero() {
+        gridTablero.setGridLinesVisible(false);
+        for (int i = 0; i < juego.getColumnas(); i++) {
+            ColumnConstraints columna = new ColumnConstraints();
+            columna.prefWidthProperty().bind(gridTablero.heightProperty().divide(juego.getFilas()));
+            gridTablero.getColumnConstraints().add(columna);
+        }
+
+        for (int i = 0; i < juego.getFilas(); i++) {
+            RowConstraints fila = new RowConstraints();
+            fila.prefHeightProperty().bind(gridTablero.widthProperty().divide(juego.getColumnas()));
+            gridTablero.getRowConstraints().add(fila);
+        }
+
+        for (int i = 0; i < juego.getFilas(); i++) {
+            for (int j = 0; j < juego.getColumnas(); j++) {
+                Pane p = new Pane();
+                if ((i + j) % 2 == 0) {
+                    p.setStyle("-fx-background-color: #C7DCFA");
+                } else {
+                    p.setStyle("-fx-background-color: #13305C");
+                }
+                gridTablero.add(p, j, i);
+            }
+        }
+    }
+
+    public void dibujarTablero() {
+        for (int i = 0; i < juego.getFilas(); i++) {
+            for (int j = 0; j < juego.getColumnas(); j++) {
+                Pane p = (Pane) gridTablero.getChildren().get(j + i * juego.getColumnas());
+                p.getChildren().clear();
+            }
+        }
+
+        for (var elemento : juego.getElementos()) {
+            Pane p = (Pane) gridTablero.getChildren().get(elemento.getY() + elemento.getX() * juego.getColumnas());
+            Label l = new Label(elemento.getNombre());
+            l.setAlignment(Pos.CENTER);
+            l.prefWidthProperty().bind(p.widthProperty());
+            l.prefHeightProperty().bind(p.heightProperty());
+            if (l.getText().equals("Jugador")) {
+                l.setStyle("-fx-background-color: #F0F0F0");
+            } else if (l.getText().equals("Fuego")) {
+                l.setStyle("-fx-background-color: #777777");
+            } else {
+                l.setStyle("-fx-background-color: #FF8000");
+            }
+            p.getChildren().add(l);
+        }
+
+    }
+}
--- src/main/java/org/robots/vista/UI.java
+++ src/main/java/org/robots/vista/UI.java
@@ -7,12 +7,20 @@ import org.robots.Main;
 import java.io.IOException;
 
 public class UI {
-    protected final int HEIGHT = 480;
-    protected final int WIDTH = 640;
+    private final int HEIGHT = 480;
+    private final int WIDTH = 640;
 
     public Parent loadFXML(String fxml, Object controlador) throws IOException {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
         loader.setController(controlador);
         return loader.load();
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public int getWIDTH() {
+        return WIDTH;
     }
 }
